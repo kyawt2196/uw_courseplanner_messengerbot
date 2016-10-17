bc5f465066eb042f82f95f847954dee3fe8ede5d
liz55 <liz55@uw.edu>
2016-10-16 17:00:28 -0700

    final version push database

--- loadCourseDatabase.py
+++ loadCourseDatabase.py
@@ -10,7 +10,7 @@ for line in file:
 
 jsonFile = json.loads(jsonString)
 
-db = client.courses
+collection = client.mixfruitdb.courses
 
 isOpen = ''
 sln = ''
@@ -27,52 +27,51 @@ isWriting = ''
 link = ''
 
 for dept in jsonFile:
-    for classList in jsonFile[dept]:
-        for className in classList:
-            for attr in className:
-                if (attr == 'meetings'):
-                    if (len(attr) == 1):
+    for classObj in jsonFile[dept]:
+        # class object is another dictionary
+        for attr in classObj:
+            if (attr == 'meetings'):
+                if ('to be arranged' in classObj[attr]):
+                    start = 'to be arranged'
+                    end = 'to be arranged'
+                    days = 'to be arranged'
+                    instructor = 'to be arranged'
+                    link = 'to be arranged'
+                else:
+                    start = classObj[attr]['start']
+                    end = classObj[attr]['end']
+                    days = classObj[attr]['day']
+                    link = 'to be arranged'
 
-                        start = 'to be arranged'
-                        end = 'to be arranged'
-                        days = 'to be arranged'
-                        instructor = 'to be arranged'
-                        link = 'to be arranged'
-                    else:
-                        start = attr['start']
-                        end = attr['end']
-                        days = attr['day']
-                        link = 'to be arranged'
+            if (attr == 'status'):
+                if (classObj[attr] == ''):
+                    isOpen = False
+                else:
+                    isOpen = True
 
-                if (attr == 'status'):
-                    if (className[attr] == ''):
-                        isOpen = False
-                    else:
-                        isOpen = True
-                
-                if (attr == 'major'):
-                    prefix = className[attr]
+            if (attr == 'major'):
+                prefix = classObj[attr]
 
-                if (attr == 'SLN'):
-                    sln = className[attr]
+            if (attr == 'SLN'):
+                sln = int(classObj[attr])
 
-                if (attr == 'professor'):
-                    instructor = className[attr]
+            if (attr == 'professor'):
+                instructor = classObj[attr]
 
-                if (attr == 'courseName'):
-                    nameOfclassName = className[attr]
+            if (attr == 'courseName'):
+                nameOfclassName = classObj[attr]
 
-                if (attr == 'courseName'):
-                    number = className[attr]
+            if (attr == 'courseNumber'):
+                number = int(classObj[attr])
 
-                isSection = True
-                isWriting = False
-            
-            result = db.insert_one(
-                {'sln':sln, 'prefix':prefix, 'number':number, 'nameOfclassName':nameOfclassName, 
-                'days':days, 'start':start, 'end':end, 'isSection':isSection, 'instructor':instructor, 
-                'isOpen':isOpen, 'generalEd':generalEd, 'isWriting':isWriting, 'link':link}
-            )
+            isSection = True
+            isWriting = False
 
-            print(result)
+        result = collection.insert_one(
+            {'sln':sln, 'prefix':prefix, 'number':number, 'nameOfclassName':nameOfclassName,
+            'days':days, 'start':start, 'end':end, 'isSection':isSection, 'instructor':instructor,
+            'isOpen':isOpen, 'generalEd':generalEd, 'isWriting':isWriting, 'link':link}
+        )
+
+        print(result)
 
